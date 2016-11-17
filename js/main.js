b3e200607e92ed3cf2b781818e012e9e84481b32
Jacob Garby <j.garby@icloud.com>
2016-11-17 17:42:21 +0000

    update

--- js/main.js
+++ js/main.js
@@ -6,6 +6,20 @@ var modal,
 	btn,
 	span
 
+var memes =       "#ed856c";
+var funny =       "#f1e292";
+var animals =     "#83816c";
+var interesting = "#96e08a";
+var sad =         "#87cbd0";
+var programming = "#50656b";
+var cordial =     "#b984c6";
+var tech =        "#c66481";
+var other =       "#e4e3e9";
+
+var categories = [ // Compile all category colours into an array
+	memes, funny, animals, interesting, sad, programming, cordial, tech, other
+];
+
 function modalInit() {
 	modal = document.getElementById( 'toolongModal' );
 
@@ -24,8 +38,6 @@ function modalInit() {
 function getRandomInt( min, max ) {
 	return Math.floor( Math.random() * ( max - min + 1 ) ) + min; // Literally just a random integer n where min <= n <= max
 }
-
-function writeToCards() {}
 
 function addCard( title, body, colour, whiteHead ) {
 	if ( !title || !body ) {
@@ -60,10 +72,17 @@ function tooDark( c ) {
 
 	var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-	if ( luma < 110 ) { // luma is basically kind of how light it is
+	if ( luma < 128 ) { // luma is basically kind of how light it is
 		return true;
 	}
 	return false;
+}
+
+function testCards() {
+	for (var i = 0; i < categories.length; i++) {
+		addCard("Lorem", "Lorem Ipsum dolor sit amet.", categories[i],
+		tooDark(categories[i]));
+	}
 }
 
 function cardFromInput() {
