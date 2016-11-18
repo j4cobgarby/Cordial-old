61616b25d765b3a29e21fa5ef36c34c6fea7f94f
Jacob Garby <j.garby@icloud.com>
2016-11-18 11:18:10 +0000

    changed comments on lightness function

--- js/main.js
+++ js/main.js
@@ -52,15 +52,15 @@ function addCard( title, body, colour, whiteHead ) {
 }
 
 function tooDark( c ) {
-	var c = c.substring( 1 ); // strip #
+	var c = c.substring( 1 ); // get rid of preceeding #
 	var rgb = parseInt( c, 16 ); // convert rrggbb to decimal
 	var r = ( rgb >> 16 ) & 0xff; // extract red
 	var g = ( rgb >> 8 ) & 0xff; // extract green
 	var b = ( rgb >> 0 ) & 0xff; // extract blue
 
-	var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+	var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-	if ( luma < 110 ) {
+	if ( luma < 110 ) { // luma is basically kind of how light it is
 		return true;
 	}
 	return false;
